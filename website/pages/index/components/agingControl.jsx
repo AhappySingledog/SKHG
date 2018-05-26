5ab50c2b04a6c9c31f442b48a57b2cf0b0d9d4b5
ecity_yumeng <yumeng@ecitychina.com>
2018-05-26 09:41:39 +0800

    1

--- website/pages/index/components/agingControl.jsx
+++ website/pages/index/components/agingControl.jsx
@@ -8,7 +8,7 @@ import echarts from 'echarts';
 // tip组件
 export default class AgingControl extends React.Component {
     state = {
-        first: true
+        layer: 'sy'
     }
     componentDidMount() {
         let ops1 = {
@@ -139,22 +139,22 @@ export default class AgingControl extends React.Component {
         this.chart1 = echarts.init(ReactDOM.findDOMNode(this.refs.echart1));
         this.chart1.setOption(ops1);
         this.chart1.on('click', (param) => {
-            this.setState({first: false});
+            this.setState({layer: 'ck', param: param.name});
         });
         if (this.chart2) this.chart2.dispose();
         this.chart2 = echarts.init(ReactDOM.findDOMNode(this.refs.echart2));
         this.chart2.setOption(ops1);
         this.chart2.on('click', (param) => {
-            this.setState({first: false});
+            this.setState({layer: 'jk', param: param.name});
         });
     }
     render() {
         return (
             <div className='ac'>
-                {this.state.first ? <div className='ac-box'>
+                {this.state.layer == 'sy' ? <div className='ac-box'>
                     <div ref='echart1' className='ac-box-t'></div>
                     <div ref='echart2' className='ac-box-b'></div>
-                </div> : <CK />}
+                </div> : <CK layer={this.state.layer} data={this.state.param}/>}
             </div>
         )
     }
@@ -163,8 +163,26 @@ export default class AgingControl extends React.Component {
 // 出口组件
 class CK extends React.Component {
     render() {
+        let datas = [
+            {layer: 'ck', style: {width: 898, height: 628}, type: 1, time: 10, items: [{name: '超去年均值', value: 2, rate: '20%'}, {name: '超目标值', value: 2, rate: '20%'}]},
+            {layer: 'ck', style: {width: 665, height: 628}, type: 0, time: 10, items: [{name: '超去年均值', value: 2, rate: '20%'}, {name: '超目标值', value: 2, rate: '20%'}]},
+            {layer: 'ck', style: {width: 773, height: 628}, type: 0, time: 10, items: [{name: '超去年均值', value: 2, rate: '20%'}, {name: '超目标值', value: 2, rate: '20%'}]},
+            {layer: 'ck', style: {width: 556, height: 628}, type: 1, time: 10, items: [{name: '超去年均值', value: 2, rate: '20%'}, {name: '超目标值', value: 2, rate: '20%'}]},
+        ];
         return (
             <div className='ac-ckbox'>
+                <div className='ac-ckbox-title'>{this.props.layer == 'ck' ? '出口': '进口'}</div>
+                <div className='ac-ckbox-t'>
+                    <div style={{background: "url('../agingControl/" + this.props.layer + ".png') no-repeat", backgroundSize: '100% 100%'}}></div>
+                    <div>
+                        {datas.map((e, i) => <JD key={i} index={i + 1} datas={e}/>)}
+                    </div>
+                </div>
+                <div className='ac-ckbox-c'><div>诊断结论：</div><div>2018年{this.props.data}出口时效......</div></div>
+                <div className='ac-ckbox-b'>
+                    <Top10 />
+                    <DataDesc />
+                </div>
             </div>
         )
     }
@@ -173,18 +191,17 @@ class CK extends React.Component {
 // 进度组件
 class JD extends React.Component {
     render() {
-        let type = this.props.type || 1;
+        let type = this.props.datas.type;
         return (
-            <div className='jd'>
-                <div style={{background: "url('../agingControl/ck_1_" + type + ".png') no-repeat", backgroundSize: '100% 100%'}}></div>
+            <div className='jd' style={this.props.datas.style}>
+                <div style={{background: "url('../agingControl/" + this.props.datas.layer + "_" + this.props.index + "_" + type + ".png') no-repeat", backgroundSize: '100% 100%'}}></div>
                 <div>
                     <div>平均时间：</div>
-                    <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}}>10</div>
+                    <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}}>{this.props.datas.time}</div>
                     <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}} >小时</div>
                 </div>
                 <div>
-                    <JDEC/>
-                    <JDEC/>
+                    {this.props.datas.items.map((e, i) => <JDEC key={i} type={this.props.datas.type} datas={e} />)}
                 </div>
             </div>
         )
@@ -229,15 +246,15 @@ class JDEC extends React.Component {
         this.chart.setOption(ops);
     }
     render() {
-        let type = this.props.type || 1;
+        let type = this.props.type;
         return (
             <div className='jd-ec'>
                 <div ref='echart'></div>
                 <div>
-                    <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}}>2</div>
+                    <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}}>{this.props.datas.value}</div>
                     <div style={{color: type == 1 ? '#70e100' : '#1f9bff'}}>小时</div>
                 </div>
-                <div>超去年均值</div>
+                <div>{this.props.datas.name}</div>
             </div>
         )
     }
@@ -251,6 +268,7 @@ class Top10 extends React.Component {
         let items = [95,90,85,80,78,76,75,74,72,70]
         return (
             <div className='top10'>
+                <div className='top10-title'>前10名-TOP10</div>
                 <div className='top10-l'></div>
                 <div className='top10-r'>
                     {items.map((e, i) => <div key={i} style={{width: e + '%', fontSize: 50, color: i == 0 ? '#ff0000' : i == 1 ? '#ee7622' : i == 2 ? '#ffad29' : 'white'}}>
